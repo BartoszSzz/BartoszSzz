220b960ddfc53c242419ba3c99e62f837b9a1bf8
BartoszSzz <bartosz.sz@onet.eu>
2016-08-10 15:31:06 +0200

    Poprawka ciągu

--- Exercises/src/ArithmeticProgression.java
+++ Exercises/src/ArithmeticProgression.java
@@ -1,9 +1,9 @@
 
 public class ArithmeticProgression {
 	public static void main(String[] args) {
-		System.out.println(Calculation(5, 2, 6));
+		System.out.println(calculation(5, 2, 6));
 	}
-	public static int Calculation(int a1, int r, int n) {
+	public static int calculation(int a1, int r, int n) {
 		int an;
 		return an = a1 + (n-1) * r;
 	}
