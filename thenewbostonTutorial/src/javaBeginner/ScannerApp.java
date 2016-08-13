3f6f5072c1217ceeede40c46a75e42fb144d5680
BartoszSzz <bartosz.sz@onet.eu>
2016-08-13 12:59:05 +0200

    Pierwsze ćwiczenie - Java scanner

---
+++ thenewbostonTutorial/src/javaBeginner/ScannerApp.java
@@ -0,0 +1,10 @@
+package javaBeginner;
+
+import java.util.Scanner;
+
+class ScannerApp {
+	public static void main(String[] args) {
+		Scanner skaner = new Scanner(System.in);
+		System.out.println(skaner.nextLine());
+	}
+}
