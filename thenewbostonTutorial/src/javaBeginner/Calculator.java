1052545aaf138e955e8fabb4446ba594b562096d
BartoszSzz <bartosz.sz@onet.eu>
2016-08-14 18:31:44 +0200

    Poprawiłem błedy w kodzie.

--- thenewbostonTutorial/src/javaBeginner/Calculator.java
+++ thenewbostonTutorial/src/javaBeginner/Calculator.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args) {
 		Scanner numbers = new Scanner(System.in);
-		Scanner operation = new Scanner(System.in);
 		double fnum, snum, result;
 		String calculationSymbol;
 		System.out.println("Podaj pierwsz¹ liczbê: ");
 		fnum = numbers.nextDouble();
 		System.out.println("Podaj drug¹ liczbê: ");
 		snum = numbers.nextDouble();
-		System.out.println("Podaj symbol dzia³ania: ");
-		calculationSymbol = operation.nextLine();
 		result = fnum + snum;
 		System.out.println("Suma  to: " + result);
 	}
