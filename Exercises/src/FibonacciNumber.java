1343d214ab8d5f8bbcdb671442f5a6113ea4bb9f
BartoszSzz <bartosz.sz@onet.eu>
2016-08-15 10:42:22 +0200

    Aplikacja jest niedokończona

---
+++ Exercises/src/FibonacciNumber.java
@@ -0,0 +1,14 @@
+
+public class FibonacciNumber {
+	public static void main(String[] args) {
+		System.out.println(calculation(4));
+	}
+	public static int calculation(int n) {
+		int f0 = 0;
+		int f1 = 1;
+		for(int i = 3; i <= n; i++) {
+			int fn; 
+		}
+		return fn
+	}
+}
