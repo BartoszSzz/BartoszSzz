11013d6e4dbbff9b0a33a4f70bee85d9c97b46b2
BartoszSzz <bartosz.sz@onet.eu>
2016-08-16 11:45:36 +0200

    Utworzyłem aplikację.

---
+++ thenewbostonTutorial/src/javaBeginner/AverageCalculator.java
@@ -0,0 +1,21 @@
+package javaBeginner;
+
+import java.util.Scanner;
+
+public class AverageCalculator {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int total = 0;
+		int number;
+		int average;
+		int counter = 1;
+		System.out.println("Podaj 10 liczb:");
+		while (counter <= 10){
+			number = input.nextInt();
+			total = total + number;
+			counter++;
+		}
+		average = total/10;
+		System.out.println("Œrednia wynosi: " + average);
+	}
+}
