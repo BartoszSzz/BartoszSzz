1edafb6e15fa713458ee1f2818aa1790d5e1e198
BartoszSzz <bartosz.sz@onet.eu>
2016-08-17 10:40:07 +0200

    Utworzyłem aplikację

---
+++ Exercises/src/AnyNumbersArray.java
@@ -0,0 +1,11 @@
+
+public class AnyNumbersArray {
+	public static void main(String[] args) {
+		System.out.println("Index\tValue");
+		int array[]={12,13,42,13,1,34};
+		
+		for(int counter=0; counter<array.length; counter++) {
+			System.out.println(counter + "\t" + array[counter]);
+		}
+	}
+}
