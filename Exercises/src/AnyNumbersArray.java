ad602946869864665b3785b41af5eb62d93dc87a
BartoszSzz <bartosz.sz@onet.eu>
2016-08-17 10:47:26 +0200

    dodałem funkcję sumowania

--- Exercises/src/AnyNumbersArray.java
+++ Exercises/src/AnyNumbersArray.java
@@ -3,9 +3,12 @@ public class AnyNumbersArray {
 	public static void main(String[] args) {
 		System.out.println("Index\tValue");
 		int array[]={12,13,42,13,1,34};
+		int sum=0;
 		
 		for(int counter=0; counter<array.length; counter++) {
+			sum+=array[counter];
 			System.out.println(counter + "\t" + array[counter]);
 		}
+		System.out.println("Suma tych liczb wynosi: " + sum);
 	}
 }
