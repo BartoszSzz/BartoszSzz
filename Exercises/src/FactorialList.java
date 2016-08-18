6ddd5d31ba84ebddfe05ef7ede36a4c04dcdda6c
BartoszSzz <bartosz.sz@onet.eu>
2016-08-18 09:42:23 +0200

    Piotrek naprawił aplikację

--- Exercises/src/FactorialList.java
+++ Exercises/src/FactorialList.java
@@ -1,19 +1,20 @@
-// ta aplikacja ma na celu wrzucenie wyników silni do tablicy - nie dzia³a
+// ta aplikacja ma na celu wrzucenie wyników silni do tablicy
 
 public class FactorialList {
 	public static void main(String[] args) {
 		int[] listArray;
-		int n = 5; // liczba, dla której obliczana jest silnia
+		int n = 16; // liczba, dla której obliczana jest silnia
 		int number = n + 1;
 		listArray = new int[number];
-		// pêtla która wyœwietla numer miejsca w tablicy i wartoœæ przypisan¹ do tego miejsca
 		
+		// pêtla która wyœwietla numer miejsca w tablicy i wartoœæ przypisan¹ do tego miejsca
 		System.out.println("Index\tNumber");
 		for(int counter = 0; counter<listArray.length; counter++) {
+			listArray[counter] = calculateFactorial(counter);
 			System.out.println(counter + "\t" + listArray[counter]);
-			}
-		
 		}
+	}
+
 		// metoda obliczaj¹ca silniê
 		public static int calculateFactorial(int n) {
 			int result = 1;
