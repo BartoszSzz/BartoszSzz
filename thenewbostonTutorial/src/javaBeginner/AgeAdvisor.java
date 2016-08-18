1feda9ccc48cd6fa5b90fb875e5adbae37fa3850
BartoszSzz <bartosz.sz@onet.eu>
2016-08-18 14:07:37 +0200

    Dodałem obsługę wyjątku

--- thenewbostonTutorial/src/javaBeginner/AgeAdvisor.java
+++ thenewbostonTutorial/src/javaBeginner/AgeAdvisor.java
@@ -1,5 +1,6 @@
 package javaBeginner;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AgeAdvisor {
@@ -7,28 +8,32 @@ public class AgeAdvisor {
 		Scanner ageScanner = new Scanner(System.in);
 		int age;
 		System.out.println("Powiedz mi ile masz lat, a ja powiem Ci w jakim jesteœ okresie rozwoju cz³owieka (podaj liczbê).");
-		age = ageScanner.nextInt();
 		
-		if(age>1) {
-			System.out.println("Jesteœ w okresie niemowlêcym"); 
-		} else if(age<=3) {
-			System.out.println("Jesteœ w okresie poniemowlêcym.");
-		} else if(age<=12) {
-			System.out.println("Jesteœ w okresie dzieciñstwa.");
-		} else if(age<=19) {
-			System.out.println("Jesteœ w okresie dojrzewania.");
-		} else if(age<=34) {
-			System.out.println("Jesteœ w okresie wczesnej doros³oœci.");
-		} else if(age<=49) {
-			System.out.println("Jesteœ w okresie œredniej doros³oœci.");
-		} else if(age<=64) {
-			System.out.println("Jesteœ w okresie póznej dros³oœci.");
-		} else if(age<=74) {
-			System.out.println("Jesteœ w podesz³ym wieku.");
-		} else if(age<=89) {
-			System.out.println("Jesteœ w starczym wieku.");
-		} else if(age>=90) {
-			System.out.println("Jesteœ w sêdziwym wieku.");
-		} 
+		try {
+			age = ageScanner.nextInt();
+			if(age<1) {
+				System.out.println("Jesteœ w okresie niemowlêcym"); 
+				} else if(age<=3) {
+					System.out.println("Jesteœ w okresie poniemowlêcym.");
+				} else if(age<=12) {
+					System.out.println("Jesteœ w okresie dzieciñstwa.");
+				} else if(age<=19) {
+					System.out.println("Jesteœ w okresie dojrzewania.");
+				} else if(age<=34) {
+					System.out.println("Jesteœ w okresie wczesnej doros³oœci.");
+				} else if(age<=49) {
+					System.out.println("Jesteœ w okresie œredniej doros³oœci.");
+				} else if(age<=64) {
+					System.out.println("Jesteœ w okresie póznej dros³oœci.");
+				} else if(age<=74) {
+					System.out.println("Jesteœ w podesz³ym wieku.");
+				} else if(age<=89) {
+					System.out.println("Jesteœ w starczym wieku.");
+				} else if(age>=90) {
+					System.out.println("Jesteœ w sêdziwym wieku.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("B³êdny format wieku. Podaj liczbê ca³kowit¹: np. 10");
+		}
 	}
 }
